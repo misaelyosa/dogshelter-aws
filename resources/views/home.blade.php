1f5c6c126cbc45e4e82b59139343a568709b78a6
Misael Yosa <107090127+misaelyosa@users.noreply.github.com>
2024-10-25 13:12:20 +0700

    add : style frosted bg page faq

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,15 +33,15 @@
         <h1 class="title2">Frequently Asked Question</h1>
         <div id="accordion-collapse" class="mb-4 relative z-20" data-accordion="collapse">
         <h2 id="accordion-collapse-heading-1">
-            <button type="button" class="accordion-head-top" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
+            <button type="button" class="accordion-head-top" data-accordion-target="#accordion-collapse-body-1" aria-expanded="false" aria-controls="accordion-collapse-body-1">
             <span>Dimana alamat shelter supaw? </span>
             <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
             </svg>
             </button>
         </h2>
-        <div id="accordion-collapse-body-1" class="hidden" aria-labelledby="accordion-collapse-heading-1">
-            <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+        <div id="accordion-collapse-body-1" class="accordion-glass hidden" aria-labelledby="accordion-collapse-heading-1">
+            <div class="p-5  ">
             <p class="mb-2 text-gray-500 dark:text-gray-400">Mohon maaf tapi kami sengaja nggak membuka lokasi dan alamat supaya nggak disalahgunakan berbagai oknum. Disalahgunakan gimana? Ya macem-macem, orang bisa makin mudah buang anjing atau kucing di shelter kami atau bahkan ada yang jahat dan nggak suka mungkin berniat
             ngeracun.Kita cuma bisa hati-hati. Semoga bisa dipahami yaa</p>
             </div>
@@ -54,8 +54,8 @@
             </svg>
             </button>
         </h2>
-        <div id="accordion-collapse-body-2" class="hidden" aria-labelledby="accordion-collapse-heading-2">
-            <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700">
+        <div id="accordion-collapse-body-2" class="accordion-glass hidden" aria-labelledby="accordion-collapse-heading-2">
+            <div class="p-5  ">
             <p class="mb-2 text-gray-500 dark:text-gray-400">Bukannya tidak boleh, namun karena kami tidak disclose alamat maka akan cukup sulit. Kedepannya mungkin bisa direncanakan, ditunggu ya.</p>
             </div>
         </div>
@@ -67,23 +67,23 @@
             </svg>
             </button>
         </h2>
-        <div id="accordion-collapse-body-3" class="hidden" aria-labelledby="accordion-collapse-heading-3">
-            <div class="p-5 border border-t-0 border-gray-200 dark:border-gray-700">
+        <div id="accordion-collapse-body-3" class="accordion-glass hidden" aria-labelledby="accordion-collapse-heading-3">
+            <div class="p-5 ">
             <p class="mb-2 text-gray-500 dark:text-gray-400">Mohon maaf tapi kami sengaja nggak membuka lokasi dan alamat supaya nggak disalahgunakan berbagai oknum. Disalahgunakan gimana? Ya macem-macem, orang bisa makin mudah buang anjing atau kucing di shelter kami atau bahkan ada yang jahat dan nggak suka mungkin berniat
 ngeracun.Kita cuma bisa hati-hati. Semoga bisa dipahami yaa</p>
             </div>
         </div>
 
         <h2 id="accordion-collapse-heading-4">
-            <button type="button" class="accordion-head-bt" data-accordion-target="#accordion-collapse-body-4" aria-expanded="false" aria-controls="accordion-collapse-body-4">
+            <button type="button" class="accordion-head-bt" data-accordion-target="#accordion-collapse-body-4" aria-expanded="true" aria-controls="accordion-collapse-body-4">
             <span>Apakah bisa menitipkan anjing di sini?</span>
             <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
             </svg>
             </button>
         </h2>
-        <div id="accordion-collapse-body-4" class="hidden" aria-labelledby="accordion-collapse-heading-4">
-            <div class="p-5 border border-t-0 border-gray-200 dark:border-gray-700">
+        <div id="accordion-collapse-body-4" class="accordion-glass hidden rounded-b-lg" aria-labelledby="accordion-collapse-heading-4">
+            <div class="p-5 ">
             <p class="mb-2 text-gray-500 dark:text-gray-400">Supaw warriors bukan tempat penitipan anjing atau kucing. Kami memiliki kapasitas yang terbatas dan tidak bersedia bertanggung jawab atas anjing dan kucing teman-teman ya. Sebaiknya dititipkan di pet hotel saja yang lebih profesional dan aman. "Gapapa, saya bayar kok." Maaf tetap tidak bisa.</p>
             </div>
         </div>
