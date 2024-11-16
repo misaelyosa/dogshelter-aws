d78048a00dc662ddd67eb6d5296524b185450e68
Misael Yosa <107090127+misaelyosa@users.noreply.github.com>
2024-11-17 02:55:41 +0700

    add : swal dependencies + usage in admin page

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,30 +1,48 @@
 @extends('base.base')
 
 @section('content')
-<section class="w-full h-full flex flex-col items-center">
-    @if (session('success'))
-    <!-- TOAST -->
-    <div id="toast-success" class="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
-        <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
-            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
-            </svg>
-            <span class="sr-only">Check icon</span>
-        </div>
-        <div class="ms-3 text-sm font-normal">{{session('success')}}</div>
-        <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-success" aria-label="Close">
-            <span class="sr-only">Close</span>
-            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-            </svg>
-        </button>
-    </div>
+<section class="w-full h-full flex flex-col items-center">  
+<!-- SWAL -->
+    @if (session('success'))  
+    <script>
+        // console.log('ballz');
+        document.addEventListener('DOMContentLoaded', function (){
+            Swal.fire({
+                icon: 'success',
+                title: 'Success',
+                text: "{{ session('success') }}",
+            });
+        });      
+    </script>   
     @endif
     
-    <div class="w-full grid gap-4 mb-4 sm:gap-6">        
-        <h1 class="font-dmsans text-black text-lg md:text-2xl font-bold mt-5  ps-6">Tabel Doge</h1>
+    <script>
+        console.log('ballz')
+        document.addEventListener('DOMContentLoaded', function (){
+        document.getElementById('btnDelete').addEventListener('click', function (){
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "This action cannot be undone!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#d33',
+                cancelButtonColor: '#3085d6',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.isConfirmed){
+                    document.getElementById('deleteForm').submit();;
+                }                
+            });
+        });
+        });
+    </script>
+<!-- END SWAL -->
+
+    <div class="w-full grid gap-4 mb-4 sm:gap-6">
+        <h1 class="font-dmsans text-black text-lg md:text-3xl font-black mt-5  ps-6">Welcome, {{Auth::user()->name}}</h1>        
+        <h1 class="font-dmsans text-black text-lg md:text-2xl font-bold ps-6">Tabel Doge</h1>
         <a href="{{route('formCreateDoge')}}"> 
-            <button type="button" class="h-[40px] ms-5 place-self-start md:place-self-end md:me-24 text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800">
+            <button type="button" class="h-[40px] ms-5 place-self-start text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800">
                 Add Doge
             </button>
         </a>
@@ -65,7 +83,7 @@
                     @foreach ($doges as $doge )
                     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                         <td class="py-4">
-                            {{ $doge->id}}
+                            {{ $loop->iteration }}
                         </td>
                         <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             {{ $doge->nama }}
@@ -91,10 +109,10 @@
                                     Edit
                                 </button>
                             </a>
-                            <a href="{{ route('deletedoge' , ['id' => $doge->id])}}">
-                                <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
-                                   Delete
-                               </button>
+                            <button id="btnDelete" type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                                Delete
+                            </button>
+                            <a id="deleteForm" href="{{ route('deletedoge' , ['id' => $doge->id])}}">
                             </a>
                         </td>
                     </tr>
