3dc4dde24e7ab67224877236ca52f5377510fbbc
Misael Yosa <107090127+misaelyosa@users.noreply.github.com>
2024-11-26 05:02:12 +0700

    fix : view home n admin

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,24 +10,35 @@ window.gsap = gsap;
 window.ScrollTrigger = ScrollTrigger;
 window.Swal = Swal;
 
-// gsap.fromTo(".loading-screen", {
-//   opacity: 1
-// },
-// {
-//   opacity: 0,
-//   duration : 2.5, //nnti ganti algo loading
-//   delay : 2,
-//   onComplete: () => {
-//     document.querySelector(".loading-screen").style.display = "none";
-//   }
-// });
+window.onload = function () {
 
-// gsap.fromTo(".loading-text",{
-//   y : 50,
-//   opacity : 0,
-// },{
-//   y: 0,
-//   opacity : 1,
-//   duration : 1.5,
-//   delay : .5,
-// });
+    gsap.fromTo(
+        ".loading-screen",
+        { opacity: 1 },
+        {
+            opacity: 0,
+            duration: 2, 
+            delay: 1.5,
+            onComplete: () => {
+                document.querySelector(".loading-screen").style.display = "none";
+                // document.querySelector("body").style.visibility = "visible";
+            },
+        }
+    );
+
+    gsap.fromTo(
+        ".loading-text",
+        { y: 50, opacity: 0 },
+        {
+            y: 0,
+            opacity: 1,
+            duration: 1.5,
+            delay: 0.5,
+        }
+    );
+};
+
+
+
+
+
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
 <section class="w-full min-h-screen flex flex-col items-center dark:bg-gray-800">  
-    <div class="w-full grid gap-2 mb-4 sm:gap-6">
+    <div class="w-full grid gap-2 mb-4 mt-10 sm:gap-6 ">
         <h1 class="justify-start font-dmsans text-black text-lg dark:text-white md:text-3xl font-black mt-5  ps-8">Welcome, {{Auth::user()->name}}</h1>
         @yield('judul')        
     </div>
